3016c494849158f7c4caed37bcd698e6b615089c
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2022-12-12 16:49:43 +0530

    Add files via upload

---
+++ basic_calc.js
@@ -0,0 +1,41 @@
+let fnum;
+let snum;
+let result;
+
+// Event Binding
+// load event will let HTML load first
+window.addEventListener("load", function() {
+    fnum = document.querySelector("#box_1");
+    snum = document.querySelector("#box_2");
+    result = document.querySelector("#box_3");
+    // document.querySelector("#add").addEventListener("click", calc);
+    // document.querySelector("#sub").addEventListener("click", calc);
+    // document.querySelector("#div").addEventListener("click", calc);
+    // document.querySelector("#mul").addEventListener("click", calc);
+
+    var btnArr = document.querySelectorAll("button");
+    for(var i = 0; i < btnArr.length; i++) {
+        btnArr[i].addEventListener("click", calc);
+    }
+});
+
+function calc() {
+    console.log(this.value);
+    // result.value = parseInt(fnum.value) + parseInt(snum.value);
+}
+
+// function add() {
+//     result.value = parseInt(fnum.value) + parseInt(snum.value);
+// }
+
+// function sub() {
+//     result.value = parseInt(fnum.value) - parseInt(snum.value);
+// }
+
+// function div() {
+//     result.value = parseInt(fnum.value) / parseInt(snum.value);
+// }
+
+// function mul() {
+//     result.value = parseInt(fnum.value) * parseInt(snum.value);
+// }
